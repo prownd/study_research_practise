00d07827c92e75a0a37cfdb1a0eb437cc0f6aa7e
prownd <545751287@qq.com>
2016-08-24 12:57:52 +0800

    add files

---
+++ nginx/nginx-rtmp-module/test/cmd.sh
@@ -0,0 +1,19 @@
+#########################################################################
+# File Name: cmd.sh
+# Author: han
+# Mail:hanjinpeng127@163.com 
+# Created Time: 2016年07月15日 星期五 13时40分54秒
+#########################################################################
+#!/bin/bash
+
+
+启动上传avi视频
+./ffmpeg
+
+#http://localhost:8080/                                                  这个链接访问的是/path/to/nginx-rtmp-module/test/www下的index.html   既有录又有播
+#http://localhost:8080/record.html  
+
+#refer
+#http://blog.csdn.net/shaoyangdd/article/details/50959297
+#http://blog.csdn.net/cjsafty/article/details/9108587
+#
